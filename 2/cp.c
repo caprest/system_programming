fdca005f2e60e6e500c2c99e0ecd181b29c69142
Fumiya Shimada <shimadafumiya@gmail.com>
2017-06-12 16:15:20 +0900

    cpっぽいものができた

---
+++ 2/cp.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#define BUFSIZE 256
+
+void recur_write(int fd,char* buf,int size_read,int wrote_size);
+void fd_check(int fd,char *name){
+        if (fd == -1){
+            printf("fd = %d, errno=%d,filename = %s\n", fd, errno,name);
+            exit(0);
+        }
+        
+        else {
+            printf("%s open succeeded\n",name);
+        }
+}
+
+void deal_write_error(int fd){
+    if(errno == EDQUOT){
+        perror("Insufficient Diskspace");
+        
+    } 
+    else{
+        perror("WRITE ERROR OCCURRED");
+    }
+ 
+    close(fd);
+    exit(0)
+}
+
+void recur_write(int fd,char* buf,int size_read,int wrote_size){
+    if(size_read == wrote_size) return;
+    else{
+        int size_write;
+        size_write = write(fd,buf,size_read-wrote_size);
+        if (size_write == -1) deal_write_error(fd);
+        else{
+        wrote_size +=size_write;
+        buf=&buf[size_write];
+        recur_write(fd,buf,size_read,wrote_size);
+        }
+    }
+}
+
+int main(int argc, char *argv[]){
+    char *in_filename = argv[1];
+    char *out_filename = argv[2];
+    if (argc!=3){
+        printf("Too many argument");
+        printf("%d",argc);
+        exit(0);
+    }
+    printf("%s",in_filename);
+    int fd_in,fd_out;
+    int i;
+    int status;
+    char buf[BUFSIZE];
+    int break_flag = 0;
+    size_t size_read = 0;
+    size_t size_write = 0;
+    //scanf("%s",namebuf);
+    fd_in = open(in_filename, O_RDONLY);
+    fd_out = open(out_filename,O_CREAT|O_RDWR);
+    fd_check(fd_in,in_filename);
+    fd_check(fd_out,out_filename);
+    while(1){
+        size_read =(int) read(fd_in,buf,BUFSIZE);
+        if (size_read<BUFSIZE) break_flag = 1;
+        if (size_read>0) {
+                recur_write(fd_out,buf,size_read,0);            
+            }
+        if (break_flag==1) break;
+    }
+
+
+    close(fd_in);
+    close(fd_out);
+}
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # test
+
+## 2
+cpの実装：
+ディスク溢れの際のエラー処理：小さなディスク領域を作る方法
+```
+dd if=/dev/zero of=fsfile bs=1024 count=1024
+mke2fs -F fsfile
+sudo mount -o loop fsfile /mnt
+```
+df fdiskなどで確認できる
+
+
+## 3
